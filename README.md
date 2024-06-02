526dfa8150fb60f4b54ac43e0080cee296adc068
Your Name <you@example.com>
2024-06-02 18:31:46 +0300

    Init

---
+++ README.md
@@ -0,0 +1,13 @@
+```sh
+npx prisma migrate dev --name init
+````
+
+```sh
+npx prisma studio
+````
+
+```sh
+npx prisma db pull
+````
+
+https://visualsql.net/
---
+++ data.sql
@@ -0,0 +1,50 @@
+insert into Sailors (sid,sname,rating,DOB)
+values(22,'Dustin',7,1/2/1999);
+insert into Sailors (sid,sname,rating,DOB)
+values(29,'Brutus',1,1/1/1999);
+insert into Sailors (sid,sname,rating,DOB)
+values(31,'Lubber',8,1/1/2000);
+insert into Sailors (sid,sname,rating,DOB)
+values(32,'Andy',8,1/5/2001);
+insert into Sailors (sid,sname,rating,DOB)
+values(58,'Rusty',10,5/5/2003);
+insert into Sailors (sid,sname,rating,DOB)
+values(64,'Horataio',7,1/12/1995);
+insert into Sailors (sid,sname,rating,DOB)
+values(71,'Zorba',10,1/12/1993);
+insert into Sailors (sid,sname,rating,DOB)
+values(74,'Horataio',9,5/9/1999);
+insert into Sailors (sid,sname,rating,DOB)
+values(85,'Art',3,8/12/2000);
+insert into Sailors (sid,sname,rating,DOB)
+values(95,'Bob',3,12/12/2000);
+
+insert into Boats (bid,bname,color)
+values(101,'Interlake','blue');
+insert into Boats (bid,bname,color)
+values(102,'Interlake','red');
+insert into Boats (bid,bname,color)
+values(103,'Clipper','green');
+insert into Boats (bid,bname,color)
+values(104,'Marine','red');
+
+insert into Reserves(sid,bid,date,timeslot)
+values(22,101,'10-OCT-98','NIGHT');
+insert into Reserves(sid,bid,date,timeslot)
+values(22,102,'10-OCT-98','DAY');
+insert into Reserves(sid,bid,date,timeslot)
+values(22,103,'8-OCT-98','DAY');
+insert into Reserves(sid,bid,date,timeslot)
+values(22,104,'7-OCT-98','NIGHT');
+insert into Reserves(sid,bid,date,timeslot)
+values(31,102,'10-Nov-98','DAY');
+insert into Reserves(sid,bid,date,timeslot)
+values(31,103,'6-NOV-98','NIGHT');
+insert into Reserves(sid,bid,date,timeslot)
+values(31,104,'12-NOV-98','NIGHT');
+insert into Reserves(sid,bid,date,timeslot)
+values(64,101,'5-SEP-98','DAY');
+insert into Reserves(sid,bid,date,timeslot)
+values(64,102,'8-SEP-98','DAY');
+insert into Reserves(sid,bid,date,timeslot)
+values(74,103,'8-march-98','DAY');
